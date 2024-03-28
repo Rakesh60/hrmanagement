0ab708679fdd595bcd900f55b34bb951f67aa8af
Rakesh60 <rakeshhupta5353@gmail.com>
2024-03-28 17:12:10 +0530

    employee form works

--- src/app/employee-reg/employee-reg.component.ts
+++ src/app/employee-reg/employee-reg.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EmployeeserviceService } from './employeeservice.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employee-reg',
   templateUrl: './employee-reg.component.html',
   styleUrl: './employee-reg.component.css'
 })
-export class EmployeeRegComponent {
+export class EmployeeRegComponent implements OnInit{
+  
+  allEmpData:any;
+  alert_msg:Boolean=false;
   employeeForm: FormGroup = new FormGroup({
     id: new FormControl(''),
     department: new FormControl('', Validators.required),
-    empName: new FormControl('', Validators.required),
+    empname: new FormControl('', Validators.required),
     mobile: new FormControl('',Validators.required),
     gender: new FormControl(''),
     joindate: new FormControl(''),
@@ -21,8 +27,31 @@ export class EmployeeRegComponent {
     empStatus: new FormControl(''),
   });
 
-  saveEmp() {
-    console.log(this.employeeForm.value)
+  constructor(private signupservice:EmployeeserviceService , private router: Router) { }
+  ngOnInit(): void {
+    this.allEmpData=this.signupservice.getallemployees;
+    console.log(this.allEmpData)
   }
+  saveEmp(): void {
+    console.log(this.employeeForm);
+    this.signupservice.createUser(this.employeeForm.value)
+      .subscribe(
+        (response: any) => {
+          console.log('Data sent successfully:', response);
+          alert(response.success);
 
+          // Optionally, reset the form
+          if (response.success) {
+            this.employeeForm.reset();
+            this.alert_msg=true;
+            //this.router.navigate(['/dashboard'])
+          }
+        },
+        (error: any) => {
+          console.error('Error sending data:', error);
+          alert(error.message)
+        }
+      );
+  }
+  
 }
--- src/app/employee-reg/employeeservice.service.ts
+++ src/app/employee-reg/employeeservice.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeserviceService {
 
-  constructor() { }
+  url = 'http://localhost:4500/api/emp/'
+  constructor(private http: HttpClient) {
+   }
+  createUser(formData: any): Observable<any> {
+    return this.http.post(`${this.url}signup`, formData);
+
+  }
+  getallemployees():Observable<any>{
+    return this.http.get(`${this.url}getallemployees`)
+  }
 }

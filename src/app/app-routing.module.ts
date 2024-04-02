61503bac27bd5e2be2d589de573c53d4bcd9e78b
Rakesh60 <rakeshhupta5353@gmail.com>
2024-04-02 15:32:58 +0530

    home added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeRegComponent } from './employee-reg/employee-reg.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'employeeform',component:EmployeeRegComponent},
-  {path:'dashboard',component:DashboardComponent}
+  {path:'dashboard',component:DashboardComponent},
+  {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
